043f09109ae5275da282f33745891f7fb14852b5
corredor29 <197847818+corredor29@users.noreply.github.com>
2026-04-07 08:18:36 -0500

    feat: :sparkles: esqueleto c#

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using MyInventory2026.src.shared.context;
+using MyInventory2026.src.shared.helpers;
+
+try
+{
+    var context = DbContextFactory.Create();
+
+
+    if (context.Database.CanConnect())
+    {
+        Console.WriteLine("Conexión exitosa a la base de datos.");
+    }
+    else
+    {
+        Console.WriteLine("No se pudo conectar con la base de datos.");
+    }
+}
+catch (Exception ex)
+{
+    Console.Error.WriteLine($"Error al conectar con la base de datos: {ex.Message}");
+    if (ex.InnerException != null)
+    {
+        Console.Error.WriteLine($"Detalle: {ex.InnerException.Message}");
+    }
+}
---
+++ src/shared/context/AppDbContext.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyInventory2026.src.shared.configuration;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
